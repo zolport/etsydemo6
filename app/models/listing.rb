f99252b05fd9945d73a00f24c38a35d3172f83ec
salomon7983@gmail.com <salomon7983@gmail.com>
2015-03-06 15:37:43 -0700

    Finish setup for dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,15 @@
 class Listing < ActiveRecord::Base
-	 has_attached_file :image, :styles => { :image => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+	if Rails.env.development?
+			 has_attached_file :image, :styles => { :image => "200x", :thumb => "100x100>" }, :default_url => "default.png"
+	else
+			has_attached_file :image, :styles => { :image => "200x", :thumb => "100x100>" }, :default_url => "default.png",	
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    :path => ":style/:id_:filename"
+end
       validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
 
 #validates_attachment_content_type :image, :content_type => { :content_type => %w(image/jpeg image/jpg image/png) } 
-#
+#In order to make rails work locally with our pictures, and in production to heroku, we did the change.
+#if Rails.env.development? and added the line :path => "style....."
