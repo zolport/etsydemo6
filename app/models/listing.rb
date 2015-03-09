326bc872c9d3eec679112139aac9041c356b9152
salomon7983@gmail.com <salomon7983@gmail.com>
2015-03-09 09:00:01 -0600

    We added validation for different fields for Users and Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,11 +3,19 @@ class Listing < ActiveRecord::Base
 			 has_attached_file :image, :styles => { :image => "200x", :thumb => "100x100>" }, :default_url => "default.png"
 	else
 			has_attached_file :image, :styles => { :image => "200x", :thumb => "100x100>" }, :default_url => "default.png",	
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    :path => ":style/:id_:filename"
+            :storage => :dropbox,
+            :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+            :path => ":style/:id_:filename"
 		end
       validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
+      validates :name, :description, :price, presence: true
+      #this line will validate that no listing will be in the system that has blanks
+      validates :price, numericality: { grather_than: 0 }
+      #this line will validate that no price will be zero and positive
+      validates_attachment_presence :image
+      #this line will validate the image field, and will not permit a blank field
 end
 
 #validates_attachment_content_type :image, :content_type => { :content_type => %w(image/jpeg image/jpg image/png) } 
