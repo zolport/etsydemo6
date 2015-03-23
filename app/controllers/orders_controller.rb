65d046a891a2ea4e72cb8d8d192c342e15ff59f2
salomon7983@gmail.com <salomon7983@gmail.com>
2015-03-23 11:11:11 -0600

    Abilitating the transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,15 +34,22 @@ end
     token = params[:stripeToken]
 
       begin
-      charge = Stripe::Charge.create(
-        :amount => (@listing.price * 100).floor,
-        :currency => "usd",
-        :card => token
-        )
-      flash[:notice] = "Thanks for ordering!"
-    rescue Stripe::CardError => e
-      flash[:danger] = e.message
-    end
+        charge = Stripe::Charge.create(
+          :amount => (@listing.price * 100).floor,
+          :currency => "usd",
+          :card => token
+          )
+              
+        flash[:notice] = "Thanks for ordering!"
+      rescue Stripe::CardError => e
+        flash[:danger] = e.message
+    end 
+
+      transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
 
     respond_to do |format|
       if @order.save
